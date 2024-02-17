5d4c9e343659c71cecaafae7fc097226d7ee602f
TOSHI <tomoki.ohnishi@fuji.waseda.jp>
2024-02-17 16:13:28 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * 加算メソッド
+ * @param x - 数値
+ * @param y - 数値
+ * @returns - 足した値
+ */
+export const add = (x: number, y: number): number => {
+  return x + y;
+};
+
+/**
+ * 減算メソッド
+ * @param x - 数値
+ * @param y - 数値
+ * @returns - 引いた値
+ */
+export const minus = (x: number, y: number): number => {
+  return x - y;
+};
